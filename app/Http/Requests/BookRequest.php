365901718fbe4b3c2e23b872a2a1ca4cd7e0edcc
RafaTicArte <rafa@ticarte.com>
2023-12-15 16:27:29 +0100

    Add code validation request. Update Thunder collection

--- app/Http/Requests/BookRequest.php
+++ app/Http/Requests/BookRequest.php
@@ -34,7 +34,7 @@ class BookRequest extends FormRequest
             'success' => false,
             'message' => 'Validation errors',
             'data' => $validator->errors(),
-        ]));
+        ], 400));
     }
 
     /**
